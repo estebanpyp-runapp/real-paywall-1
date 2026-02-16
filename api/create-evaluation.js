2fdf22fd498ba6e82e21d6a369cf6f8be56a66cd
estebanpyp-runapp <estebanpyp@gmail.com>
2026-02-16 16:42:30 -0600

    Update create-evaluation.js

--- api/create-evaluation.js
+++ api/create-evaluation.js
@@ -1,3 +1,6 @@
 module.exports = async (req, res) => {
-  return res.status(200).json({ message: "Function works" })
+  return res.status(200).json({
+    url: process.env.SUPABASE_URL ? "OK" : "MISSING",
+    key: process.env.SUPABASE_SERVICE_ROLE_KEY ? "OK" : "MISSING"
+  })
 }
